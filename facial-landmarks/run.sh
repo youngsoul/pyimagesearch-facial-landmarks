38260bdd59b7d1cf0fe0c192d8ed07ca6044f83a
patrickryan <theyoungsoul@gmail.com>
2020-06-18 20:59:26 -0500

    update run script with right example image

--- facial-landmarks/run.sh
+++ facial-landmarks/run.sh
@@ -1,1 +1,1 @@
-python facial_landmarks.py --shape-predictor ./pyimagesearch-facial-landmarks/shape_predictor_68_face_landmarks.dat --image ./pyimagesearch-facial-landmarks/images/example_05.jpg
+python facial_landmarks.py --shape-predictor ./pyimagesearch-facial-landmarks/shape_predictor_68_face_landmarks.dat --image ./pyimagesearch-facial-landmarks/images/example_03.jpg
